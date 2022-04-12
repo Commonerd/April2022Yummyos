6f9abdae5efd5a2ffefa543979f62130eba1bcd1
commonerd <cornflake127@naver.com>
2022-04-12 18:19:10 +0900

    20220412(화)_송영화_위시리스트 띄우기,맛집정보dto~controller,내일은 맛집정보를 위시리스트에 insert하기 구현하도록

---
+++ src/main/java/app/yummyos/store/dto/StoreDto.java
@@ -0,0 +1,18 @@
+package app.yummyos.store.dto;
+
+import lombok.Data;
+
+@Data
+public class StoreDto {
+		private int no;
+		private String name;
+		private String category;
+		private String opentime;
+		private String address;
+		private double map_x;
+		private double map_y;
+		private long viewcount;
+		
+
+	}
+
--- src/main/webapp/WEB-INF/views/users/main.jsp
+++ src/main/webapp/WEB-INF/views/users/main.jsp
@@ -126,7 +126,8 @@
 	<a href="logout">로그아웃</a>
 	<a href="board/notice/list">공지사항</a>
 	
-	<a href="wishlists">위시리스트</a>
+	<a href="wishlist">위시리스트</a>
+	<a href="storedetail">맛집 상세보기</a>
 	<br>
 
 	<!-- <iframe
