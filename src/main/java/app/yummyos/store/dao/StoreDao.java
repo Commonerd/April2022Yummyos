a8f8d56e5325df4bd6b0af89bc20213328c831cf
commonerd <cornflake127@naver.com>
2022-04-13 18:24:32 +0900

    20220413(수)_공공데이터 넣기, 위리스트 실패..

--- src/main/java/app/yummyos/store/dao/StoreDao.java
+++ src/main/java/app/yummyos/store/dao/StoreDao.java
@@ -1,13 +1,20 @@
 package app.yummyos.store.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import app.yummyos.board.notice.dto.NoticeDto;
 import app.yummyos.store.dto.StoreDto;
 
 @Mapper
 public interface StoreDao {
-
 		int viewStore(StoreDto dto);
+		List<StoreDto> storeList(Map<String, Object> m);
+		int count();//전체글 갯수
+		StoreDto StoreOne(int no);
+		int updateStore(Store dto);
 		/*int insertUsers(UsersDto dto);
 		UsersDto login(UsersDto dto);
 		int updateUsers(UsersDto dto);
